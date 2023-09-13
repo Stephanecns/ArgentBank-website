1eba27996deec16559a55303f5399f3e96bdd8ad
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2023-09-13 02:50:45 +0200

    Ajout de @reduxjs/toolkit + Provide the Redux Store to React

--- bank-app/src/index.js
+++ bank-app/src/index.js
@@ -5,12 +5,16 @@ import ReactDOM from 'react-dom/client';
 import '../src/styles/index.css'; 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from '../src/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <BrowserRouter>
+      <Provider store={store}>
     <App />
+    </Provider>,
     </BrowserRouter>
   </React.StrictMode>
 );
