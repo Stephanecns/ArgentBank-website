091e76fe243c28847fde839fbf61f1336ba37a62
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2023-09-15 14:03:40 +0200

    Privateroute pour la page user

---
+++ bank-app/src/components/PrivateRoute.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+
+//Dans cette configuration, le hook useEffect dans PrivateRoute vérifiera à chaque rendu si un token est présent dans le store Redux. Si un token n'est pas présent, il redirigera l'utilisateur vers la page de connexion. Si un token est présent, il rendra les enfants (dans ce cas, <UserPage />).
+function PrivateRoute({ children }) {
+  const token = useSelector((state) => state.login.token);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!token) {
+      navigate('/sign-in');
+    }
+  }, [token, navigate]);
+
+  return token ? children : null;
+}
+
+export default PrivateRoute;
+
