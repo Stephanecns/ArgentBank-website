072ce76dfed3f6ddbc97539da7b3ae56f5ae7612
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2023-09-15 14:42:08 +0200

    Mise en place d'un msg d'erreur en cas d'identifiant incorrect

--- bank-app/src/components/SignInContent.js
+++ bank-app/src/components/SignInContent.js
@@ -1,18 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { fetchLogin } from '../redux/loginSlice'; 
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { fetchLogin } from "../redux/loginSlice";
 
 function SignInContent() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const status = useSelector((state) => state.login.status);
-
-  const [credentials, setCredentials] = useState({ email: '', password: '' });
+  const error = useSelector((state) => state.login.error);
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setCredentials((prevCredentials) => ({ ...prevCredentials, [name]: value }));
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = (e) => {
@@ -21,8 +24,8 @@ function SignInContent() {
   };
 
   useEffect(() => {
-    if (status === 'succeeded') {
-      navigate('/user');
+    if (status === "succeeded") {
+      navigate("/user");
     }
   }, [status, navigate]);
 
@@ -32,18 +35,33 @@ function SignInContent() {
       <h1>Sign In</h1>
       <form onSubmit={handleSubmit}>
         <div className="input-wrapper">
-        <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" value={credentials.email} onChange={handleChange} />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={credentials.email}
+            onChange={handleChange}
+          />
         </div>
         <div className="input-wrapper">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" value={credentials.password} onChange={handleChange} />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={credentials.password}
+            onChange={handleChange}
+          />
         </div>
         <div className="input-remember">
           <input type="checkbox" id="remember-me" />
           <label htmlFor="remember-me">Remember me</label>
         </div>
-        <button type="submit" className="sign-in-button">Sign In</button>
+        <button type="submit" className="sign-in-button">
+          Sign In
+        </button>
+        {error && <div className="error-message">Email ou mot de passe incorrect</div>}
       </form>
     </section>
   );
@@ -51,5 +69,4 @@ function SignInContent() {
 
 export default SignInContent;
 
-
-//Green code = oui 
+//Green code = oui
--- bank-app/src/redux/loginSlice.js
+++ bank-app/src/redux/loginSlice.js
@@ -1,35 +1,34 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 // Création du thunk fetchLogin pour gérer la connexion de l'utilisateur de manière asynchrone
 export const fetchLogin = createAsyncThunk(
-  'login/fetchLogin',
+  "login/fetchLogin",
   async (credentials, { rejectWithValue }) => {
-    console.log('Credentials:', credentials); 
+    console.log("Credentials:", credentials);
     try {
       // Envoie une requête POST avec les identifiants de l'utilisateur à l'API pour tenter une connexion
-      const response = await fetch('http://localhost:3001/api/v1/user/login', {
-        method: 'POST',
+      const response = await fetch("http://localhost:3001/api/v1/user/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(credentials),
       });
-      console.log('Response:', response);
-      
+      console.log("Response:", response);
+
       // Vérifie que la réponse est OK, sinon lance une erreur
       if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
+        throw new Error("Network response was not ok " + response.statusText);
       }
 
       // Parse la réponse en JSON
       const data = await response.json();
-      console.log('Data:', data);
+      console.log("Data:", data);
       // Stockage du token après avoir vérifié que la réponse est OK et après avoir parse le JSON
-      localStorage.setItem('token', data.body.token);
+      localStorage.setItem("token", data.body.token);
 
-      return data;      
-    }
-     catch (error) {
+      return data;
+    } catch (error) {
       // En cas d'erreur, nous renvoyons le message d'erreur pour le gérer dans le slice
       return rejectWithValue(error.message);
     }
@@ -38,30 +37,30 @@ export const fetchLogin = createAsyncThunk(
 
 // État initial du slice
 const initialState = {
-  token: localStorage.getItem('token') || null, // Récupère le token du localStorage s'il existe, sinon attribue `null` comme valeur par défaut.
-  status: 'idle', // status représente l'état actuel de la requête (idle, loading, succeeded, failed)
+  token: localStorage.getItem("token") || null, // Récupère le token du localStorage s'il existe, sinon attribue `null` comme valeur par défaut.
+  status: "idle", // status représente l'état actuel de la requête (idle, loading, succeeded, failed)
   error: null, // error sera utilisé pour stocker tout message d'erreur renvoyé par l'API
 };
 
 // Création du slice avec createSlice
 const loginSlice = createSlice({
-  name: 'login', // Nom du slice
+  name: "login", // Nom du slice
   initialState, // État initial du slice
   reducers: {
-    resetLoginState: () => initialState, 
-  }, 
+    resetLoginState: () => initialState,
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchLogin.pending, (state) => {
-        state.status = 'loading'; // Met à jour le statut lorsqu'une requête est en cours
+        state.status = "loading"; // Met à jour le statut lorsqu'une requête est en cours
       })
       .addCase(fetchLogin.fulfilled, (state, action) => {
-        state.status = 'succeeded'; 
+        state.status = "succeeded";
         // Supposant que votre payload a une structure { status, message, body }
-        state.token = action.payload.body.token; 
-      })      
+        state.token = action.payload.body.token;
+      })
       .addCase(fetchLogin.rejected, (state, action) => {
-        state.status = 'failed'; // Met à jour le statut en cas d'échec
+        state.status = "failed"; // Met à jour le statut en cas d'échec
         state.error = action.error.message; // Stocke le message d'erreur
       });
   },
